910571bac3c76fd5bcb29c3e1a6b3fdd77f8f937
Denis.Torgov <deniska-3000@bk.ru>
2023-03-18 09:47:25 +0300

    task_5 completed

---
+++ seminar_5/mazeGen.java
@@ -0,0 +1,135 @@
+// Генератор лабиринта для волнового алгоритма
+
+package seminar_5;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.random.*;
+
+public class mazeGen {
+    public static void printarr (int[][] xa) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < xa.length; i++) {
+            for (int j = 0; j < xa[i].length; j++) {
+                sb.append(xa[i][j] + ", ");
+            }
+            System.out.println(sb);
+        sb.setLength(0);
+        }
+    }
+    public static void printMaze(int[][] vert, int[][] horiz) {
+        StringBuilder sb = new StringBuilder();
+        System.out.print(" ");
+        for (int i = 0; i < horiz[0].length; i++) {
+            System.out.print("_ ");
+        }
+        System.out.println();
+        for (int i = 0; i < horiz.length; i++) {
+            sb.append("|");
+            for (int j = 0; j < horiz[i].length; j++) {
+                if (horiz[i][j] == 1) {
+                    sb.append("_ ");
+                } else {
+                    sb.append("  ");
+                }
+                if (vert[i][j] == 1) {
+                    sb.setLength(sb.length() - 1);
+                    sb.append("|");
+                }
+            }
+            System.out.println(sb);
+            sb.setLength(0);
+        }
+    }
+
+    public static int[][] fillLine(int[][] line, int row, int step) {
+        for (int j = 0; j < line[row].length; j++) {
+            if (line[row][j] == 0) {
+                line[row][j] = step;
+                step +=1;
+            }
+        }
+        return line;
+    }
+    public static int[][] copyLine(int[][] line, int row) {
+        for (int j = 0; j < line[row].length; j++) {
+           line[row+1][j] = line[row][j];
+        }
+        return line;
+    }
+    public static int stepNumb(int[][] line, int row, int x) {
+        int count = 0;
+        for (int j = 0; j < line[row].length; j++) {
+            if (line[row][j] == x) {
+                count +=1;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] arg) {
+    
+    Random random = new Random();
+    int[][] mazevert = new int[4][5]; //{{0,0,0,1},{1,0,1,1},{0,1,0,1},{0,0,0,1}};
+    int[][] mazehoriz = new int[4][5]; //{{1,0,1,0},{0,0,1,0},{1,1,0,1},{1,1,1,1}};
+    int[][] maze = new int[4][5];
+    
+    int step = 1;
+    // HashSet<Integer> setmaze = new HashSet<>();
+    // setmaze.add(1);
+    maze = fillLine(maze, 0, step);
+    step = maze[0][maze[0].length-1];
+    for (int i = 0; i < maze.length; i++) {
+        for (int j = 0; j < maze[i].length -1; j++) {
+            if (random.nextInt(10) <= 5) {
+                if (maze[i][j] == maze[i][j+1]) {
+                    mazevert[i][j] = 1;
+                } else {
+                    mazevert[i][j] = 0;
+                    maze[i][j+1] = maze[i][j];
+                }
+            } else {
+                mazevert[i][j] = 1;
+            }
+        }
+        mazevert[i][mazevert[i].length-1] = 1;
+        int exit = 0; 
+        for (int j = 0; j < maze[i].length; j++) {
+            int setline = stepNumb(maze, i, maze[i][j]);
+            if (setline == 1) {
+                mazehoriz[i][j] = 0;
+            } else {
+                if (random.nextInt(10) <= 5) {
+                    if (setline > 1 && exit == 0) {
+                        mazehoriz[i][j] = 1;
+                        exit = 1;
+                    }
+                }
+            }
+        }
+        if (i < maze.length - 1) {
+            maze = copyLine(maze, i);
+            for (int j = 0; j < maze[i+1].length; j++) {
+                if (mazehoriz[i][j] == 1) {
+                    maze[i+1][j] = 0;
+                }
+            }
+        int maxline = step;
+        for (int j = 0; j < maze.length; j++) {
+            if (maxline < maze[i][j]) {
+                maxline = maze[i][j];
+            }
+        }
+        maze = fillLine(maze, i+1, maxline + 1);
+        }
+    // for (int j = 0; j < maze[i].length -1; j++) {
+    //     mazehoriz[i][j] = 1;    
+    // }
+    }
+    
+    System.out.println(0x5e);
+    printMaze(mazevert, mazehoriz);
+    printarr(maze);
+    printarr(mazehoriz);
+
+    }
+}
